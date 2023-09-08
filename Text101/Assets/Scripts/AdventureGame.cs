a49adb53185b9f801fe6c519c9437dfa0c466cde
justinhnguyen <justinn7@usc.edu>
2023-09-07 23:01:41 -0700

    Test

--- Text101/Assets/Scripts/AdventureGame.cs
+++ Text101/Assets/Scripts/AdventureGame.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AdventureGame : MonoBehaviour
 {
+   
+    public Text textComponent;
     // Start is called before the first frame update
     void Start()
     {
-        
+   
     }
 
     // Update is called once per frame
