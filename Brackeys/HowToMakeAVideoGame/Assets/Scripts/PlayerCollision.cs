97b886fb32a82a688d89f8fe9523c554d2521ec7
justinhnguyen <justinn7@usc.edu>
2023-09-30 17:07:58 -0700

    added music

--- Brackeys/HowToMakeAVideoGame/Assets/Scripts/PlayerCollision.cs
+++ Brackeys/HowToMakeAVideoGame/Assets/Scripts/PlayerCollision.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour {
 
 	public PlayerMovement movement;     // A reference to our PlayerMovement script
+	public AudioSource;
 
 	// This function runs when we hit another object.
 	// We get information about the collision and call it "collisionInfo".
@@ -11,6 +12,11 @@ public class PlayerCollision : MonoBehaviour {
 		// We check if the object we collided with has a tag called "Obstacle".
 		if (collisionInfo.collider.tag == "Obstacle")
 		{
+			if (collisionSound != null)
+            {
+				collisionSound.Play();
+            }
+
 			movement.enabled = false;   // Disable the players movement.
 			FindObjectOfType<GameManager>().EndGame();
 		}
