188d5f477aba97a5ae83d2e27ff8962cdfa46cea
justinhnguyen <justinn7@usc.edu>
2023-10-02 16:09:58 -0700

    Adjusted script to add backwards force

--- Brackeys/HowToMakeAVideoGame/Assets/Scripts/PlayerMovement.cs
+++ Brackeys/HowToMakeAVideoGame/Assets/Scripts/PlayerMovement.cs
@@ -27,6 +27,10 @@ public class PlayerMovement : MonoBehaviour {
 			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 		}
 
+		if (Input.GetKey("s"))
+        {
+			forwardForce = 0f;
+        }
 		if (rb.position.y < -1f)
 		{
 			FindObjectOfType<GameManager>().EndGame();
