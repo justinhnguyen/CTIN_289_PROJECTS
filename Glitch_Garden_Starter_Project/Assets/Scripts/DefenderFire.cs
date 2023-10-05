ee9527dce4667d248ff4a7d9bc21df47c73cc004
justinhnguyen <justinn7@usc.edu>
2023-10-04 22:57:16 -0700

    Spawning, projectiles, health

---
+++ Glitch_Garden_Starter_Project/Assets/Scripts/DefenderFire.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderFire : MonoBehaviour
+{
+    [SerializeField] GameObject projectile, gun;
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, transform.rotation);
+    }
+}
