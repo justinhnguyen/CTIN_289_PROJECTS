9cc67f47f9ccd925f9961189e1a15e627bad52c4
justinhnguyen <justinn7@usc.edu>
2023-10-04 23:41:35 -0700

    Added UI art

--- Glitch_Garden_Starter_Project/Assets/Scripts/DefenderSpawner.cs
+++ Glitch_Garden_Starter_Project/Assets/Scripts/DefenderSpawner.cs
@@ -13,11 +13,19 @@ public class DefenderSpawner : MonoBehaviour
     private Vector2 GetSquareClicked()
     {
         Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        Vector2 worldPOs = Camera.main.ScreenToWorldPoint(clickPos);
-        return worldPOs;
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        Vector2 gridPos = SnapToGrid(worldPos);
+        return gridPos;
     }
-    private void SpawnDefender(Vector2 worldPos)
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
     {
-        GameObject newDefender = Instantiate(defender, worldPos, Quaternion.identity) as GameObject;
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+        return new Vector2 (newX, newY);    
+    }
+    private void SpawnDefender(Vector2 roundedPos)
+    {
+        GameObject newDefender = Instantiate(defender, roundedPos, Quaternion.identity) as GameObject;
     }
 }
