51917e971ba678c16f9cb34767fdeced0d536567
justinhnguyen <justinn7@usc.edu>
2023-10-18 16:27:57 -0700

    Added camera and player controller

--- Mr Suplex/Mr Suplex/Assets/Scripts/PlayerController.cs
+++ Mr Suplex/Mr Suplex/Assets/Scripts/PlayerController.cs
@@ -2,19 +2,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float moveSpeed;
-
+    [SerializeField] float speed;
+    [SerializeField] Vector3 moveDirection;
+    [SerializeField] Vector3 jumpVelocity;
+    [SerializeField] Animator animator;
     CharacterController ch;
+
+    //Jump
+    [SerializeField] float jumpHeight = 1.5f;
 
     void Start()
     {
         ch = GetComponent<CharacterController>();
     }
 
-    private void Update()
+    void Update()
     {
-        float x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        ch.Move(new Vector3 (x, 0, z));
+        float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        moveDirection = new Vector3(horizontal, 0, vertical);
+        moveDirection *= speed;
+
+        if (moveDirection == Vector3.zero)
+        {
+            animator.SetFloat("Speed", 0);
+        }
+        else if (!Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetFloat("Speed", 0.5f);
+        }
+        else
+            animator.SetFloat("Speed", 1);
+        
     }
 }
